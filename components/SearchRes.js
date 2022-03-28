fa6b340571538437925a6b55e4443870b7ff3874
tavassoliachi <tavassoliachi@gmail.com>
2022-03-28 17:17:24 +0400

    ..

--- components/SearchRes.js
+++ components/SearchRes.js
@@ -17,39 +17,47 @@ const SearchRes = ({ data }) => {
       getAvatar(data.uid, setUID);
     }
   }, []);
+  const handlePress = () => {
+    data.uid == currUid
+      ? navigation.navigate("profile")
+      : (navigation.push("searchProfile", {
+          search: true,
+          uid: data.uid,
+        }),
+        dispatch(getUserData(data.uid)));
+  };
   return (
-    <TouchableOpacity
-      style={{ marginTop: 10, flexDirection: "row", alignItems: "center" }}
-      onPress={() =>
-        data.uid == currUid
-          ? navigation.navigate("profile")
-          : (navigation.push("searchProfile", {
-              search: true,
-              uid: data.uid,
-            }),
-            dispatch(getUserData(data.uid)))
-      }
-    >
+    <TouchableOpacity style={styles.mainCont} onPress={handlePress}>
       <Image
         size={45}
-        style={{
-          width: 45,
-          height: 45,
-          borderRadius: 45 / 2,
-          backgroundColor: "#c4c4c4",
-          marginRight: 20,
-        }}
+        style={styles.image}
         source={{
           uri:
             uid[data.uid] ||
             "https://bombyxplm.com/wp-content/uploads/2021/01/421-4213053_default-avatar-icon-hd-png-download.png",
         }}
       />
-      <Text style={{ fontWeight: "600" }}>{data.username}</Text>
+      <Text style={styles.username}>{data.username}</Text>
     </TouchableOpacity>
   );
 };
 
 export default SearchRes;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  mainCont: {
+    marginTop: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  image: {
+    width: 45,
+    height: 45,
+    borderRadius: 45 / 2,
+    backgroundColor: "#c4c4c4",
+    marginRight: 20,
+  },
+  username: {
+    fontWeight: "600",
+  },
+});
